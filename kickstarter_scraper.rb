a0e7900ba9a09402a85a3fb7b0674133d1ac53b8
Henry Sztanski <henry@sztanski.com>
2019-03-26 03:35:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,16 @@ def create_project_hash
 
   kickstarter = Nokogiri::HTML(html)
   binding.pry
+
+  project = kickstarter.css("li.project.grid_4")
+
+  #title: project.css("h2.bbcard_name strong a").text
+  #image link: project.css("div.project-thumbnail a img").attribute("src").value
+  #description: project.css("p.bbcard_blurb").text
+  #location: project.css("span.location-name").text
+  #percent_funded: project.css("li.first.funded strong").text
+  
+  
 end
 
 create_project_hash
